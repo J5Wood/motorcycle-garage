c4003408ea696a62aa420ec62afcc2cd05d8b3db
J5WOOD <js.wood206@gmail.com>
2020-12-03 21:37:23 -0800

    Only shows login or signup page if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/login' do
-        erb :'login'
+        if  !self.logged_in?
+            erb :'login'
+        else
+            redirect "/users/#{session[:user_id]}"
+        end
     end
 
     post '/login' do
@@ -23,17 +27,26 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/signup' do
-        erb :'signup'
+        if !self.logged_in?
+            erb :'signup'
+        else
+            redirect "users/#{session[:user_id]}"
+        end
     end
 
     post '/signup' do
-        if !User.find_by(name: params[:username])
+        if !User.find_by(name: params[:username]) && !params[:username].empty? && !params[:password].empty?
             user = User.create(name: params[:username], password: params[:password])
             session[:user_id] = user.id
             redirect "/users/#{user.id}"
         else
             redirect "/signup"
         end
+    end
+    
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,5 +29,7 @@ class UsersController < ApplicationController
         end
         redirect "/users/#{@user.id}"
     end
+
+    
         
 end
