ea7dd654a0af2362c90d999ea2b7fb00e949d3cf
J5WOOD <js.wood206@gmail.com>
2020-12-04 21:57:59 -0800

    Made new motorcycle form

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -5,6 +5,37 @@ class MotorcyclesController < ApplicationController
         erb :'motorcycles/index'
     end
 
+    get '/motorcycles/new' do
+        @brands = Brand.all
+        erb :'motorcycles/new'
+    end
+
+    post '/motorcycles' do
+        if params[:year].to_i.between?(1885, DateTime.now.year + 1) && !params[:name].empty?
+            motorcycle = Motorcycle.new(name: params[:name].upcase, year: params[:year].to_i)
+        else
+            redirect '/motorcycles/new'
+        end
+        if !params[:color].empty?
+            motorcycle.color = params[:color]
+        end
+        if !params[:mileage].empty?
+            motorcycle.mileage = params[:mileage]
+        end
+        if !params[:new_brand].empty?
+            brand = Brand.find_by(name: params[:brand])      #Make work with multiple words
+            if !brand
+                motorcycle.brand = Brand.create(name: params[:new_brand].downcase.capitalize)
+            else
+                motorcycle.brand = brand
+            end
+        elsif !!params[:brand]
+            motorcycle.brand = Brand.find_by(name: params[:brand])
+        end
+        motorcycle.save
+        redirect "/motorcycles/#{motorcycle.id}" 
+    end
+
     get '/motorcycles/:id' do
         @motorcycle = Motorcycle.find_by_id(params[:id])
         erb :'motorcycles/show'
