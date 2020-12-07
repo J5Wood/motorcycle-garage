52ee1fdd443279d919f35d314cfad3c475818ba3
J5WOOD <js.wood206@gmail.com>
2020-12-06 23:32:31 -0800

    Added brand controller and views

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,7 +1,57 @@
 class BrandsController < ApplicationController
 
+    get '/brands' do
+        @brands = Brand.all
+        erb :'brands/index'
+    end
+
+    post '/brands' do
+        if params[:name].empty?
+            redirect '/brands/new'
+        end
+        brand = Brand.find_by(name: params[:name])
+        if !brand
+            brand = Brand.create(name: params[:name])
+        else
+            redirect "/brands/#{brand.id}"
+        end
+        if !params[:year].empty?
+            brand.year = params[:year]
+        end
+        if !params[:headquarters].empty?
+            brand.headquarters = params[:headquarters]
+        end
+        brand.save
+        redirect "/brands/#{brand.id}"
+    end
+
+    get '/brands/new' do
+        erb :'brands/new'
+    end
+
     get '/brands/:id' do
         @brand = Brand.find_by_id(params[:id])
         erb :'brands/show'
     end
+
+    get '/brands/:id/edit' do
+        @brand = Brand.find_by_id(params[:id])
+        erb :'brands/edit'
+    end
+
+    patch '/brands/:id' do
+        brand = Brand.find_by_id(params[:id])
+        if !params[:name].empty?
+            brand.name = params[:name]
+        end
+        if !params[:year].empty?
+            brand.year = params[:year]
+        end
+        if !params[:headquarters].empty?
+            brand.headquarters = params[:headquarters]
+        end
+        brand.save
+        redirect "/brands/#{brand.id}"
+    end
+
 end
--- db/migrate/20201201022622_create_brands.rb
+++ db/migrate/20201201022622_create_brands.rb
@@ -2,6 +2,8 @@ class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
       t.string :name
+      t.integer :year
+      t.string :headquarters
     end
   end
 end
