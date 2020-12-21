04da7538a2fff36d541caf0933d69ef8c00b0b27
Jeremy Wood <js.wood206@gmail.com>
2020-12-21 15:46:40 -0800

    Protected user patch and delete routes, cleaned up user patch and delete get routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,18 +21,16 @@ class UsersController < ApplicationController
     get '/users/:id/edit' do
         self.redirect_if_not_logged_in
         self.redirect_if_bad_route(self.env["REQUEST_PATH"].split("/")[1][0...-1].capitalize)
-
-        if session[:user_id] == params[:id].to_i
-            @user = User.find_by_id(session[:user_id])
-            erb :'users/edit'
-        else
-            flash[:message] = "You May Only Alter Your Own Profile"
-            redirect '/'
-        end
+        
+        @user = User.find_by_id(params[:id])
+        redirect_if_incorrect_user(@user)
+        erb :'users/edit'
     end
 
     patch '/users/:id' do
         @user = User.find_by_id(params[:id])
+        redirect_if_incorrect_user(@user)
+
         if params[:password_one] != params[:password_two]
             flash[:message] = "Passwords Must Match"
             redirect "/users/#{@user.id}/edit"
@@ -59,19 +57,28 @@ class UsersController < ApplicationController
         self.redirect_if_not_logged_in
         self.redirect_if_bad_route(self.env["REQUEST_PATH"].split("/")[1][0...-1].capitalize)
 
-        @user = User.find_by_id(session[:user_id])
-        if @user.id == params[:id].to_i
-            erb :'users/delete'
-        else
-            flash[:message] = "You May Only Delete Your Own Profile!!!"
-            redirect "/users/home"
-        end
+        @user = User.find_by_id(params[:id])
+        redirect_if_incorrect_user(@user)
+
+        erb :'users/delete'
     end
 
     delete '/users/:id' do
         user = User.find_by_id(params[:id])
+        redirect_if_incorrect_user(user)
         user.destroy
         session.clear
         redirect "/"
+    end
+
+
+    helpers do
+
+        def redirect_if_incorrect_user(user)
+            if session[:user_id] != user.id
+                flash[:message] = "You May Only Alter Your Own Profile"
+                redirect "/users/home"
+            end
+        end
     end
 end
